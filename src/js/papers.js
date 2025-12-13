9da340923605f1aa387cf35d1bfbea6bd2a1b562
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-12-13 13:20:25 +0200

    123

--- src/js/papers.js
+++ src/js/papers.js
@@ -3,6 +3,11 @@ const scissors = document.querySelector("#scissors");
 const paperRef = document.querySelector("#paper");
 
 
+const resultText = document.querySelector(".paper__win");
+const computerText = document.querySelector('[data-score="computer"]');
+const playerText = document.querySelector('[data-score="player"]');
+
+
 const options = ["stone", "scissors", "paper"];
 
 let player = 0;
@@ -14,3 +19,16 @@ function getRandomElement(arr) {
     return arr[index]
 }
 
+function playRound(playerChoice){
+
+  const compputerChoice = getRandomElement(options)
+
+  if(playerChoice === compputerChoice){
+    resultText.textContent = "Нічія";
+  }
+
+  const playerWin = 
+  (playerChoice === "stone")
+  (playerChoice === "scissors")
+  (playerChoice === "paper")
+}
